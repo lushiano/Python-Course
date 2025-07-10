d1cce572b52b21d384cae31cb29644fb8c4e12ff
luisemezeta <ldi.luis.munoz@gmail.com>
2025-07-09 19:10:20 -0600

    Added lesson operators

--- Lessons/02_operators.py
+++ Lessons/02_operators.py
@@ -7,21 +7,21 @@ print(3 + 4)
 print(3 - 4)
 print(3 * 4)
 print(3 / 4)
-print(10 % 3)
-print(10 // 3)
-print(2 ** 3)
+print(10 % 3) #operador de modulo 10 divided by 3 is 3 with a remainder of 1
+print(10 // 3) # operador floor division 10 divided by 3 the result is an int number
+print(2 ** 3) # operador exponencial 2 elevado a 3
 print(2 ** 3 + 3 - 7 / 1 // 4)
 
 # Operaciones con cadenas de texto
 print("Hola " + "Python " + "¿Qué tal?")
-print("Hola " + str(5))
+print("Hola " + str(5)) # hay que definir el tipo de variable cuando son dos distintas
 
 # Operaciones mixtas
 print("Hola " * 5)
 print("Hola " * (2 ** 3))
 
 my_float = 2.5 * 2
-print("Hola " * int(my_float))
+print("Hola " * int(my_float)) # se multiplica por una variable de numero entero
 
 ### Operadores Comparativos ###
 
